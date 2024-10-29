be4391588549b4ec1ba3e30908a36d4877785f1e
“Katleenmm” <katleen.mendes@outlook.com>
2024-10-28 22:11:42 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { FormsModule } from '@angular/forms';
+import { NgFor, NgIf, UpperCasePipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [FormsModule, NgFor, NgIf, UpperCasePipe, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular_chat';
+
+  novoItem: string = '';
+
+  lista: string[] = [];
+
+  adicionaItem() {
+    this.lista.push(this.novoItem);
+    this.novoItem = '';
+  }
+
+  removeItem(item: string) {
+    this.lista = this.lista.filter((i) => i !== item);
+  }
 }
